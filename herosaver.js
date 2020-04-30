8185eb98266f0042aba2bca6fed203a76ced8162
Javitel <javiervt251@gmail.com>
2020-04-30 20:30:24 +0200

    Update herosaver.js

--- herosaver.js
+++ herosaver.js
@@ -1,7 +1,3 @@
-//download.js v4.2, by dandavis; 2008-2017. [MIT] see http://danml.com/download.html for tests/usage
-(function(r,l){"function"==typeof define&&define.amd?define([],l):"object"==typeof exports?module.exports=l():r.download=l()})(this,function(){return function l(a,e,k){function q(a){var h=a.split(/[:;,]/);a=h[1];var h=("base64"==h[2]?atob:decodeURIComponent)(h.pop()),d=h.length,b=0,c=new Uint8Array(d);for(b;b<d;++b)c[b]=h.charCodeAt(b);return new f([c],{type:a})}function m(a,b){if("download"in d)return d.href=a,d.setAttribute("download",n),d.className="download-js-link",d.innerHTML="downloading...",d.style.display="none",document.body.appendChild(d),setTimeout(function(){d.click(),document.body.removeChild(d),!0===b&&setTimeout(function(){g.URL.revokeObjectURL(d.href)},250)},66),!0;if(/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//.test(navigator.userAgent))return/^data:/.test(a)&&(a="data:"+a.replace(/^data:([\w\/\-\+]+)/,"application/octet-stream")),!window.open(a)&&confirm("Displaying New Document\n\nUse Save As... to download, then click back to return to this page.")&&(location.href=a),!0;var c=document.createElement("iframe");document.body.appendChild(c),!b&&/^data:/.test(a)&&(a="data:"+a.replace(/^data:([\w\/\-\+]+)/,"application/octet-stream")),c.src=a,setTimeout(function(){document.body.removeChild(c)},333)}var g=window,b=k||"application/octet-stream",c=!e&&!k&&a,d=document.createElement("a");k=function(a){return String(a)};var f=g.Blob||g.MozBlob||g.WebKitBlob||k,n=e||"download",f=f.call?f.bind(g):Blob;"true"===String(this)&&(a=[a,b],b=a[0],a=a[1]);if(c&&2048>c.length&&(n=c.split("/").pop().split("?")[0],d.href=c,-1!==d.href.indexOf(c))){var p=new XMLHttpRequest;return p.open("GET",c,!0),p.responseType="blob",p.onload=function(a){l(a.target.response,n,"application/octet-stream")},setTimeout(function(){p.send()},0),p}if(/^data:([\w+-]+\/[\w+.-]+)?[,;]/.test(a)){if(!(2096103.424<a.length&&f!==k))return navigator.msSaveBlob?navigator.msSaveBlob(q(a),n):m(a);a=q(a),b=a.type||"application/octet-stream"}else if(/([\x80-\xff])/.test(a)){e=0;var c=new Uint8Array(a.length),t=c.length;for(e;e<t;++e)c[e]=a.charCodeAt(e);a=new f([c],{type:b})}a=a instanceof f?a:new f([a],{type:b});if(navigator.msSaveBlob)return navigator.msSaveBlob(a,n);if(g.URL)m(g.URL.createObjectURL(a),!0);else{if("string"==typeof a||a.constructor===k)try{return m("data:"+b+";base64,"+g.btoa(a))}catch(h){return m("data:"+b+","+encodeURIComponent(a))}b=new FileReader,b.onload=function(a){m(this.result)},b.readAsDataURL(a)}return!0}});
-
-
 function init() {
     (function(){
 
@@ -17,26 +13,23 @@ function init() {
             var normalMatrixWorld = new THREE.Matrix3();
 
             return function ( scenes ) {
-				
                 console.log(scenes);
-				
                 var output = '';
-				
                 output += 'solid exported\n';
-				
                 for(var scene_nr in scenes) {
-					
                     scenes[scene_nr].traverse( function ( object ) {
-						
                         if(object instanceof RK.Mesh){		    
                             // if object is hidden - exit
-                            if(object.visible == false) return; 
-
+                            if(object.visible == false) return;
                             var geometry = object.geometry;
                             var matrixWorld = object.matrixWorld;
                             var skeleton = object.skeleton;
                             var mesh = object;
-
+				console.log('Found Mesh' + mesh.name);
+		            if(typeof onlyExportThisMesh !== 'undefined' && mesh.name != onlyExportThisMesh) {
+				    console.log('Looking for :' + onlyExportThisMesh + ' therefore ignoring ' + mesh.name);
+				    return;
+			    }
                             if(geometry instanceof RK.BufferGeometry){
 								//Get pose from skeleton
                                 var bufferGeometry = geometry.clone();
@@ -60,13 +53,9 @@ function init() {
                                     for(var i = 0; i < geometry.vertices.length; i++) {
                                         geometry.skinIndices.push((new THREE.Vector4 ()).fromBufferAttribute(skinIndex,i));
                                         geometry.skinWeights.push((new THREE.Vector4 ()).fromBufferAttribute(skinWeight,i));
-                                        for(var i = 0; i < geometry.vertices.length; i++) {
-                                        	geometry.skinIndices.push((new THREE.Vector4 ()).fromBufferAttribute(skinIndex,i));
-                                        	geometry.skinWeights.push((new THREE.Vector4 ()).fromBufferAttribute(skinWeight,i));
-                                        	for(var j = 0; j < mtcount; j++) {
-                                         	   geometry.morphTargets[j].vertices.push((new THREE.Vector3 ()).fromBufferAttribute(oldgeometry.getAttribute('morphTarget' + j)));
-                                        	}
-                                    	}
+                                        for(var j = 0; j < mtcount; j++) {
+                                            geometry.morphTargets[j].vertices.push((new THREE.Vector3 ()).fromBufferAttribute(oldgeometry.getAttribute('morphTarget' + j)));
+                                        }
                                     }
                                 }
                             }
@@ -75,7 +64,6 @@ function init() {
 
                                 var vertices = geometry.vertices;
                                 var faces = geometry.faces;
-
                                 normalMatrixWorld.getNormalMatrix( matrixWorld );
 
                                 if(typeof faces != 'undefined'){
@@ -144,7 +132,7 @@ function init() {
 
                                                 var finalVector = new THREE.Vector4();
 
-                                                if (geometry.morphTargets !== 'undefined') {
+                                                if (mesh.geometry.morphTargets !== 'undefined') {
 
                                                     var morphVector = new THREE.Vector4(vector.x, vector.y, vector.z);
 
@@ -156,17 +144,14 @@ function init() {
                                                 }
 
                                                 for (var k = 0; k < 4; k++) {
-                                                    if (geometry.morphTargets !== 'undefined') {
-                                                        var tempVector = new THREE.Vector4(morphVector.x, morphVector.y, morphVector.z);
-                                                    } else {
-                                                        var tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
-                                                    }                                                    
-                                                    
+                                                     var tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
                                                     tempVector.multiplyScalar(weights[k]);
                                                     //the inverse takes the vector into local bone space
-													//which is then transformed to the appropriate world space
-                                                    tempVector.applyMatrix4(inverses[k]).applyMatrix4(skinMatrices[k]);
+                                                    tempVector.applyMatrix4(inverses[k])
+                                                    //which is then transformed to the appropriate world space
+                                                        .applyMatrix4(skinMatrices[k]);
                                                     finalVector.add(tempVector);
+
 
                                                 }
 
